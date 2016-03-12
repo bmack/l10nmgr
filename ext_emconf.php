541bcb344afa8c8bf9447162867a2cdce36313f6
Jo Hasenau <jo.hasenau@external.telekom.de>
2016-03-12 14:39:56 +0100

    [TASK] Adjusted version numbers and compatibility for CMS 7
    This version will be running on TYPO3 CMS 7.6.x LTS only
    Support will end according to TYPO3 7.6 LTS lifetime

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
     'description' => 'Module for managing localization import and export',
     'category' => 'module',
 	'shy' => 0,
-    'version' => '5.0.0-dev',
+    'version' => '7.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
             'depends' =>
                 array(
                     'php' => '5.5.0-0.0.0',
-                    'typo3' => '6.2.0-7.99.99',
+                    'typo3' => '7.6.0-7.6.99',
                 ),
             'conflicts' =>
                 array(),
